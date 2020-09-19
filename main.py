c95dfd7767ada01dd89f291c87f31b1223c0c49e
Aidan Copinga <u0651651@utah.edu>
2020-09-19 14:06:42 -0600

    Fixed Main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -6,6 +6,7 @@ def home_page():
     if request.method=='GET':
         return "<h1>homepage</h1>"
     else:
+        return None
         # handle image upload here
 
 @app.route('/about')
